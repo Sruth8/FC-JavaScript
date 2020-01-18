6680e5c6fcba63313595faff03d38ba7eee72f9e
Shelisha Rutherford <starshipazul@yahoo.com>
2020-01-18 17:52:17 -0500

    case

--- vars.js
+++ vars.js
@@ -2,10 +2,41 @@
 
 var a; //declairing a variable
 var b = 2; // assigning 2 to b
-
+console.log(a)
 a = 7;
 b = a;
 
 console.log(a)
+
+
+//Initializing Variables with Assignment operater
+
+var a = 9;
+
+// Uninitialized variables
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + "string";
+
+// Case Sensitivity in variables
+
+  //Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar =10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+console.log(studlyCapVar)
+// adding Numbers
+
 
 
