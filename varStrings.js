a7812c78f461d66f91a8be97b601a3f013e287ad
Shelisha Rutherford <starshipazul@yahoo.com>
2020-01-20 22:34:02 -0500

    strings

--- varStrings.js
+++ varStrings.js
@@ -3,3 +3,10 @@ var firstName = "Drew";
 var lastName = "Condor";
 
 //Escaping Literal Quates in Strings
+var myString = "I'm a \"double quoted\" string inside \"double quotes\"";
+console.log(myString)
+
+//Quoting Strings with Single Qoutes
+var mySingleQstring = '<a href="http://www.expample.com" target="_blank">Link</a>';
+
+//Escape Sequences in s
