cfac4b877ab68ee17884bd3eb9fc69c5ecc21fd8
Shelisha Rutherford <starshipazul@yahoo.com>
2020-01-21 21:15:18 -0500

    more strings

--- varStrings.js
+++ varStrings.js
@@ -9,4 +9,40 @@ console.log(myString)
 //Quoting Strings with Single Qoutes
 var mySingleQstring = '<a href="http://www.expample.com" target="_blank">Link</a>';
 
-//Escape Sequences in s
+//Escape Sequences in Strings
+ /*****
+  CODE OUTPUT
+  \'  single quote
+  \"  double quote
+  \\  backslash
+  \n  newline
+  \r  carriage return
+  \t  tab
+  \b  backspace
+  \f  form feed
+
+  *****/
+
+  var myStrg = "FirstLine\n\t\\SecondLine\nThirdLine"
+  console.log(myStrg)
+
+  //Concatenating Strings with Plus Operator
+  var ourStr = "I come first. " + "I come second. ";
+    console.log(ourStr)
+
+ //Concatenating Strings with Plus Equals Operator
+ var ourStrig = "I come first. ";
+ ourStrig += "I come second.";
+   console.log(ourStrig)
+  
+//Constructing Strings with Variables
+var  myName = "Nikkie";
+var  myStrName = "Hello, my name is " + myName + ", Whatz good?";  
+   console.log(myStrName)
+
+// Appending Variables to Strings
+var  anAdjective = "awesome!";
+var  ourStr = "freeCodeCamp is " ;
+ourStr += anAdjective;
+   console.log(ourStr)
+
