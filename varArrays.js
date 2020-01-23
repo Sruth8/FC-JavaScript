61a3682b38f1da8269901851d04878c767ecf728
Shelisha Rutherford <starshipazul@yahoo.com>
2020-01-22 21:21:19 -0500

    push-pop

--- varArrays.js
+++ varArrays.js
@@ -33,3 +33,22 @@ var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 var myData = myArray[2][1]; //first 0 will get the first element in the array. [1,2,3]
                             //the second will get the number inside the []
 console.log(myData)                            
+
+//Manipulate Arrays with push()
+var ourArray = ["Garfield", "R", "cat"];
+ourArray.push(["happy", "joy"]); //this will push this element into the array shown above
+   console.log(ourArray)
+
+var myArray = [["John", 24], ["mouse", 2]];
+myArray.push(["dog", 4])
+  console.log(myArray)
+
+//Manipulate Arrays with pop()
+var  ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); //popped off the 3
+   console.log(ourArray)
+   
+var myArray = [["Dude", 22], ["rat", 3]];
+var removedFromMyArray = myArray.pop(); //popped off the ["rat", 3]
+   console.log(myArray)   
+
