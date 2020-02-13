5135892e60c6985398f53f50a1e0228a82e24561
Shelisha Rutherford <starshipazul@yahoo.com>
2020-02-12 22:07:23 -0500

    Return values

--- functions.js
+++ functions.js
@@ -35,21 +35,62 @@ fun2();
 
 //------- Local Scope and Functions-----
 function myLocalScope() {
-
+ var myVar = 3;
     console.log(myVar);
 
 }
 
 myLocalScope();
 
-console.log(myVar); //<--- this will not work. you cant't access "myVar" outside the function
+//console.log(myVar); //<--- this will not work. you cant't access "myVar" outside the function
+
+
 
 //------ Global vs. Local Scope in Function------------
 var outerWear = "T-Shirt";   //you can have local and Global varibles with the same name
-
-function myOutfit() {
-
+                             
+function myOutfit() { 
+   var outerWear = "pants"; //The local takes priority over Global
     return outerWear;
 }
 
 console.log(myOutfit());
+console.log(outerWear); // this is logged 2nd
+
+//-------- Return a Value from a function --------
+ function minusSeven(num) {
+     return num -7;
+ }
+
+ console.log(minusSeven(10)); //this should log a 3
+
+ function miltiEight(num) {
+    return num * 8;
+}
+
+console.log(miltiEight(5)); //this should log a 40
+
+//---------Understanding Undefined Value Returned from a Function---------//
+  var sum = 0;           
+  function addThree() {
+      sum += 3; //sum = sum + 3; //no return statement logs out undifined. must specify a return value to work
+  }
+
+// ---------Assignment with a Returned Value ----------
+ var changed = 0;  //intilized "changed" here
+   function change(num) {
+       return (num + 5) / 3;
+   } 
+
+  changed = change(10); //then the value of the return will stored here in "changed"
+  
+  var processed = 0;
+   function processArg(num) {
+       return (num + 3) / 5;
+   }
+processed = processArg(7);
+
+//----------Stand in Line--------
+
+
+
