476e273df16c2ee8442c4041a58b2402c58c54db
Shelisha Rutherford <starshipazul@yahoo.com>
2020-02-12 20:24:20 -0500

    Global and Local

--- functions.js
+++ functions.js
@@ -1,23 +1,28 @@
-// Write Reuseable Code with Functions
+//-------Write Reuseable Code with Functions--------
 function myReusableFunction() {
     console.log("Yoooo, Winning");
 }
 
 myReusableFunction(); //calling the function here
 
-//Passing Values to Functions with Arguments
+//---------Passing Values to Functions with Arguments--------
 function myFunctionWtihArgs(a, b) {
     console.log(a - b); //output 5
     console.log(a + b); //output 15
 }
    myFunctionWtihArgs(10, 5);// Output 5
 
-//Global Scope and Functions. 
+//---------Global Scope and Functions.---------- 
                               // Scope refers to the visiablity of varables out side of the function block
                               // Global means your varable can be found anywhere in the javascript code.
-function fun2() {
+var myGlobal = 10;
+
+function fun1(){
+ oopsGlobal = 5; //putting this here without the "var" makes this statment Global
+}
+    function fun2() {
     var output = "";
-    if (typeof myGlobal != "undefined") {
+    if (typeof myGlobal != "undefined") {       // != means does not equal
         output += "myGlobal: " + myGlobal;
     }
     if (typeof oopsGlobal != "undefined") {
@@ -25,3 +30,26 @@ function fun2() {
     }
     console.log(output);
 }                              
+fun1();
+fun2();
+
+//------- Local Scope and Functions-----
+function myLocalScope() {
+
+    console.log(myVar);
+
+}
+
+myLocalScope();
+
+console.log(myVar); //<--- this will not work. you cant't access "myVar" outside the function
+
+//------ Global vs. Local Scope in Function------------
+var outerWear = "T-Shirt";   //you can have local and Global varibles with the same name
+
+function myOutfit() {
+
+    return outerWear;
+}
+
+console.log(myOutfit());
